71538ed1b509c9db3f0209624dc184db8f56ddd2
shreesha <shreeshapoojary100@gmail.com>
2026-09-10 20:02:08 +0530

    fixed syntax

--- patterns.c
+++ patterns.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 
-void pattern1(){
-    for(int i=0;i<5;i++){
+void pattern1()
+    {
+        for(int i=0;i<5;i++){
+    
         for(int j=0;j<5;j++){
             printf("*");
         }
         printf("\n");
     }
-}
+    }
+
+    
+    void pattern2()
+    {
+    for(int i=0;i<5;i++){    
+        for(int j=0;j<i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    }
+
+
+
 int main() {
     pattern1();
+    pattern2();
     return 0;
 }
