abbe06dafd701553587abb06982066b9b91c015a
shreesha <shreeshapoojary100@gmail.com>
2026-09-10 19:43:45 +0530

    added first c file

---
+++ patterns.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void pattern1(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main() {
+    pattern1();
+    return 0;
+}
