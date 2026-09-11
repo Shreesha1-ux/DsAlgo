d703a18640b4c0b3ed74c232d8ce48400ebb6416
shreesha <shreeshapoojary100@gmail.com>
2026-09-11 07:14:32 +0530

    added pattern6

--- patterns.c
+++ patterns.c
@@ -30,8 +30,48 @@ void pattern1()
             printf("\n");
         }
     }
+     void pattern4(){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                printf("%d",i);
+            }
+            printf("\n");
+        }
+    }
+    void pattern5(){
+        for(int j=0;j<5;j++){
+            for(int i=5;i>j;i--)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+   void pattern6(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5-i-1;j++){
+                printf(" ");
 
+            }
+            for(int j=0;j<2*i+1;j++){
+                printf("*");
+
+
+            }
+
+
+        for(int j=0;j<5-i-1;j++){
+                printf(" ");
+              }
+        
+        printf("\n");
+            }
+    }
+    
+    
 int main() {
-    pattern3();
+    pattern6();
     return 0;
 }
+
+
